84374413dec67c3de634ea358da16430aa09eb49
sherif <sherifahmedelsayed2@gmail.com>
2022-11-09 01:36:48 +0200

    UI modifications

--- streamlit_functions.py
+++ streamlit_functions.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 def onPlayButtonClick():
     st.session_state["play_state"] = not st.session_state["play_state"]
+
+
+
--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import streamlit_functions as functions
-import plotly.express as px
 import streamlit_vertical_slider as svs
 import librosa
+import wave
+import numpy as np
 import scipy.fft as fft
-import soundfile as sf
+import plotly.express as px
+
+# ---------------------- Websites Options -------------------------------- #
+st.set_page_config(page_title="Equalizer",layout="wide",page_icon="🎚",initial_sidebar_state="expanded")
 
 # ---------------------- Elements Styling -------------------------------- #
-
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
@@ -18,51 +21,57 @@ if "play_state" not in st.session_state:
 # ---------------------- Uploading Files --------------------------------- #
 
 st.sidebar.markdown("# Upload")
-uploaded_file = st.sidebar.file_uploader("uploader",key="uploaded_file",label_visibility="hidden")
+uploaded_file = st.sidebar.file_uploader("uploader",key="uploaded_file",label_visibility="hidden",type="wav")
 
-# ---------------------- Play/Pause Button ------------------------------- #
 
-play_button = st.sidebar.button(label= "PLAY" if not st.session_state["play_state"] else "PAUSE",
-                                disabled= not st.session_state["uploaded_file"], 
-                                on_click= functions.onPlayButtonClick)
+if st.session_state["uploaded_file"]:
+# ---------------------- Play/Pause Audio -------------------------------- #
+
+    st.sidebar.audio(st.session_state["uploaded_file"],"wav")
+
 # ---------------------- Sliders ----------------------------------------- #
-columns = st.columns(10)
-for column in columns:
-    with column:
-        svs.vertical_slider(key = f"slider{columns.index(column)}", 
-                            min_value=-100,
-                            max_value=100,
-                            step=1,
-                            default_value=0,
-                            thumb_color="#2481ce",
-                            slider_color="#061724",
-                            track_color="lightgray")
 
-# ------------------------------------------------------------------------ #
-st.markdown("***")
-# ---------------------- Graph ------------------------------------------- #
+    columns = st.columns(10)
+    for column in columns:
+        with column:
+            svs.vertical_slider(key = f"slider{columns.index(column)}", 
+                                min_value=-100,
+                                max_value=100,
+                                step=1,
+                                default_value=0,
+                                thumb_color="#2481ce",
+                                slider_color="#061724",
+                                track_color="lightgray")
 
-data, sample_rate = librosa.load('dub.wav')
-transformed = fft.rfft(data)[:len(data)//2]
-print(transformed)
-inversed = fft.irfft(transformed)
+# -----------------------line break -------------------------------------- #
+    st.markdown("***")
+# ---------------------- Plots ------------------------------------------- #
 
-export = st.sidebar.button("Export")
-if export:
-    sf.write("inversed.wav",inversed,sample_rate)
+    signal_plot_col, spectrogram_col = st.columns(2)
+    with signal_plot_col:
+        audio         = wave.open(st.session_state["uploaded_file"], 'rb')
+        sample_rate = audio.getframerate()
+        n_samples   = audio.getnframes()
+        duration    = n_samples / sample_rate
+        signal_wave = audio.readframes(-1)
 
+        signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
+        signal_x_axis = np.arange(0,duration,1/(2*sample_rate))
 
-st.sidebar.audio("inversed.wav")
+        yf = fft.rfft(signal_y_axis)
+        xf = fft.rfftfreq(round(sample_rate*duration),1/sample_rate)
 
-fig1 = px.line(inversed)
-fig1.update_layout(showlegend=True, margin=dict(l=0, r=0, t=0, b=0), legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
-fig1.update_xaxes(showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-fig1.update_yaxes(showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-st.plotly_chart(fig1)
+        modified_signal = fft.irfft(yf)
 
-subt = [m - n for m,n in zip(data,inversed)]
-fig = px.line(subt)
-fig.update_layout(showlegend=True, margin=dict(l=0, r=0, t=0, b=0), legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
-fig.update_xaxes(showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-fig.update_yaxes(showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-st.plotly_chart(fig)
+        modified_signal_channel = np.int16(modified_signal * (32767 / modified_signal.max()))
+
+        fig = px.line(x = signal_x_axis,y = signal_y_axis)
+        fig['data'][0]['showlegend'] = True
+        fig['data'][0]['name'] = 'Original'
+        fig.add_scatter(name="Modified", x=signal_x_axis,y=modified_signal, line_color="#FF4B4B",visible="legendonly")
+        fig.update_layout(showlegend=True, margin=dict(l=0, r=0, t=0, b=0), legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+        fig.update_xaxes(title="Time",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
+        fig.update_yaxes(title="Amplitude",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
+        st.plotly_chart(fig,use_container_width=True)
+    with spectrogram_col:
+        st.plotly_chart(fig,use_container_width=True)
