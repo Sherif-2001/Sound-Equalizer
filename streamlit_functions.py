6738838bfce4112688dfe984073a13356b6a1eb0
sherif <sherifahmedelsayed2@gmail.com>
2022-11-09 04:22:21 +0200

    add functionality to sliders

--- streamlit_functions.py
+++ streamlit_functions.py
@@ -1,7 +1,48 @@
+import numpy as np
+import scipy.fft as fft
+import plotly.express as px
+import wave
 import streamlit as st
+from scipy.io.wavfile import write
 
-def onPlayButtonClick():
-    st.session_state["play_state"] = not st.session_state["play_state"]
+def signalTransform():
+    if st.session_state["uploaded_file"]:
+        audio   =     wave.open(st.session_state["uploaded_file"], 'rb')
+        sample_rate = audio.getframerate()
+        n_samples   = audio.getnframes()
+        duration    = n_samples / sample_rate
+        signal_wave = audio.readframes(-1)   
 
+        signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
+        signal_x_axis = np.arange(0,duration,1/(2*sample_rate))
 
+        yf = fft.rfft(signal_y_axis)
+        xf = fft.rfftfreq(len(yf),1/sample_rate)
 
+        ranges = np.arange(0,np.abs(xf.max()),np.abs(xf.max())/10)
+
+        for i in range(10):
+            if i < 9:
+                yf[int(duration*ranges[i]):int(duration* ranges[i+1])] *= st.session_state.get(f"slider{i+1}")
+            else:
+                yf[int(duration*ranges[-1]):int(duration* xf.max())] *= st.session_state.get(f"slider10")
+
+        modified_signal = fft.irfft(yf)
+
+        modified_signal_channel = np.int16(modified_signal)
+        write("Modified.wav", sample_rate * 2, modified_signal_channel)
+
+        return signal_x_axis,signal_y_axis,modified_signal
+    else:
+        return np.arange(0,1,0.1),np.zeros(10),np.zeros(10)
+
+def plotSignals():
+    signal_x_axis,signal_y_axis,modified_signal = signalTransform()
+    signal_figure = px.line(x = signal_x_axis,y = signal_y_axis)
+    signal_figure['data'][0]['showlegend'] = True
+    signal_figure['data'][0]['name'] = 'Original'
+    signal_figure.add_scatter(name="Modified", x=signal_x_axis,y=modified_signal, line_color="#FF4B4B",visible="legendonly")
+    signal_figure.update_layout(showlegend=True, margin=dict(l=0, r=0, t=0, b=0), legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+    signal_figure.update_xaxes(title="Time",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
+    signal_figure.update_yaxes(title="Amplitude",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
+    return signal_figure
--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import streamlit_functions as functions
 import streamlit_vertical_slider as svs
-import librosa
-import wave
-import numpy as np
-import scipy.fft as fft
-import plotly.express as px
 
 # ---------------------- Websites Options -------------------------------- #
 st.set_page_config(page_title="Equalizer",layout="wide",page_icon="🎚",initial_sidebar_state="expanded")
@@ -14,64 +9,46 @@ st.set_page_config(page_title="Equalizer",layout="wide",page_icon="🎚",initial
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# ---------------------- Declaring Variables State ----------------------- #
-if "play_state" not in st.session_state:
-    st.session_state["play_state"] = False
-
 # ---------------------- Uploading Files --------------------------------- #
 
-st.sidebar.markdown("# Upload")
 uploaded_file = st.sidebar.file_uploader("uploader",key="uploaded_file",label_visibility="hidden",type="wav")
 
+# ---------------------- Original Audio ---------------------------------- #
 
-if st.session_state["uploaded_file"]:
-# ---------------------- Play/Pause Audio -------------------------------- #
-
-    st.sidebar.audio(st.session_state["uploaded_file"],"wav")
+st.sidebar.markdown("# Original Signal")
+st.sidebar.audio(st.session_state["uploaded_file"] if st.session_state["uploaded_file"] else None,"wav")
 
 # ---------------------- Sliders ----------------------------------------- #
 
-    columns = st.columns(10)
-    for column in columns:
-        with column:
-            svs.vertical_slider(key = f"slider{columns.index(column)}", 
-                                min_value=-100,
-                                max_value=100,
-                                step=1,
-                                default_value=0,
-                                thumb_color="#2481ce",
-                                slider_color="#061724",
-                                track_color="lightgray")
+columns = st.columns(10)
+for column in columns:
+    if f"slider{columns.index(column)+1}" not in st.session_state:
+        st.session_state[f"slider{columns.index(column)+1}"] = 1
+
+for column in columns:
+    with column:
+        svs.vertical_slider(key = f"slider{columns.index(column)+1}", 
+                            min_value=0,
+                            max_value=5,
+                            step=1,
+                            default_value=1,
+                            thumb_color="#2481ce",
+                            slider_color="#061724",
+                            track_color="lightgray")
 
 # -----------------------line break -------------------------------------- #
-    st.markdown("***")
+st.markdown("***")
 # ---------------------- Plots ------------------------------------------- #
 
-    signal_plot_col, spectrogram_col = st.columns(2)
-    with signal_plot_col:
-        audio         = wave.open(st.session_state["uploaded_file"], 'rb')
-        sample_rate = audio.getframerate()
-        n_samples   = audio.getnframes()
-        duration    = n_samples / sample_rate
-        signal_wave = audio.readframes(-1)
+signal_plot_col, spectrogram_col = st.columns(2)
 
-        signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
-        signal_x_axis = np.arange(0,duration,1/(2*sample_rate))
+with signal_plot_col:
+    st.plotly_chart(functions.plotSignals(),use_container_width=True)
+# with spectrogram_col:
+    # st.plotly_chart(signal_figure,use_container_width=True)
+    # f,t,Sxx = sig.spectrogram(modified_signal,sample_rate)
+    # px.density_heatmap(Sxx)
 
-        yf = fft.rfft(signal_y_axis)
-        xf = fft.rfftfreq(round(sample_rate*duration),1/sample_rate)
-
-        modified_signal = fft.irfft(yf)
-
-        modified_signal_channel = np.int16(modified_signal * (32767 / modified_signal.max()))
-
-        fig = px.line(x = signal_x_axis,y = signal_y_axis)
-        fig['data'][0]['showlegend'] = True
-        fig['data'][0]['name'] = 'Original'
-        fig.add_scatter(name="Modified", x=signal_x_axis,y=modified_signal, line_color="#FF4B4B",visible="legendonly")
-        fig.update_layout(showlegend=True, margin=dict(l=0, r=0, t=0, b=0), legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
-        fig.update_xaxes(title="Time",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-        fig.update_yaxes(title="Amplitude",showline=True, linewidth=2, linecolor='black',gridcolor='#5E5E5E', title_font=dict(size=24, family='Arial'))
-        st.plotly_chart(fig,use_container_width=True)
-    with spectrogram_col:
-        st.plotly_chart(fig,use_container_width=True)
+# ---------------------- Modified Signal --------------------------------- #
+st.sidebar.markdown("# Modified Signal")
+st.sidebar.audio("Modified.wav" if st.session_state["uploaded_file"] else None,"wav")
